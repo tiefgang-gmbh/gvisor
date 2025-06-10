5448aed79ceb9e4e4deef1e8e71a9f903dc62088
Nayana Bidari <nybidari@google.com>
2025-06-10 15:47:17 -0700

    Fix the link endpoint panic.
    The panic was happening as the endpoint was trying to send packets with nil
    dispatcher. Do not send packets when dispatcher is nil.
    
    Before: http://sponge2/a609cccc-714a-4a83-b488-e52ea59618f9
    After: http://sponge2/8cec0b14-7820-4937-8b28-2ecfaecfa773
    PiperOrigin-RevId: 769833561
    (cherry picked from commit c6c0e528b9df7c5f25385c7d8f01f3963a91c374)

--- pkg/tcpip/link/pipe/pipe.go
+++ pkg/tcpip/link/pipe/pipe.go
@@ -57,7 +57,10 @@ type Endpoint struct {
 }
 
 func (e *Endpoint) deliverPackets(pkts stack.PacketBufferList) {
-	if !e.linked.IsAttached() {
+	e.linked.mu.RLock()
+	d := e.linked.dispatcher
+	e.linked.mu.RUnlock()
+	if d == nil {
 		return
 	}
 
@@ -68,9 +71,6 @@ func (e *Endpoint) deliverPackets(pkts stack.PacketBufferList) {
 		newPkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
 			Payload: pkt.ToBuffer(),
 		})
-		e.linked.mu.RLock()
-		d := e.linked.dispatcher
-		e.linked.mu.RUnlock()
 		d.DeliverNetworkPacket(pkt.NetworkProtocolNumber, newPkt)
 		newPkt.DecRef()
 	}
